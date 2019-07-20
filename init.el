29a17d9b2fc11f7f112d6a3b0aa8d7f5bf90d014
bhutton <ben@benhutton.com.au>
2019-07-20 11:36:39 +0800

    Updated theme

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
   (package-refresh-contents))
 
 ; list the packages you want
-(setq package-list '(better-defaults solarized-theme helm helm-projectile helm-ag ruby-electric rvm seeing-is-believing chruby inf-ruby ruby-test-mode))
+(setq package-list '(better-defaults helm helm-projectile helm-ag ruby-electric rvm seeing-is-believing chruby inf-ruby ruby-test-mode))
 
 
 ; install the missing packages
@@ -26,10 +26,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-day)))
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#d6d6d6" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#4d4d4c"))
+ '(beacon-color "#c82829")
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes
    (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+    ("392395ee6e6844aec5a76ca4f5c820b97119ddc5290f4e0f58b38c9748181e8d" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "170bb47b35baa3d2439f0fd26b49f4278e9a8decf611aa33a0dad1397620ddc3" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "0dd2666921bd4c651c7f8a724b3416e95228a13fca1aa27dc0022f4e023bf197" "d5aec3a39364bc4c6c13f472b2d0cdaebd5cff7a6e4839749be2156fcc075006" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
+ '(fci-rule-color "#d6d6d6")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(frame-background-mode (quote light))
+ '(hl-sexp-background-color "#efebe9")
  '(hl-todo-keyword-faces
    (quote
     (("TODO" . "#dc752f")
@@ -47,10 +56,36 @@
      ("FIXME" . "#dc752f")
      ("XXX+" . "#dc752f")
      ("\\?\\?\\?+" . "#dc752f"))))
+ '(nrepl-message-colors
+   (quote
+    ("#183691" "#888a88" "#539100" "#888a88" "#0086b3" "#183691" "#539100" "#888a88")))
  '(package-selected-packages
    (quote
-    (bundler projectile-rails exec-path-from-shell spacemacs-theme ruby-refactor emr dumb-jump rspec-mode centaur-tabs auto-complete-exuberant-ctags ## auto-complete color-theme-sanityinc-tomorrow better-defaults)))
- '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef"))))
+    (flatui-theme sublime-themes color-theme-sanityinc-solarized leuven-theme one-themes chyla-theme bundler projectile-rails exec-path-from-shell spacemacs-theme ruby-refactor emr dumb-jump rspec-mode centaur-tabs auto-complete-exuberant-ctags ## auto-complete color-theme-sanityinc-tomorrow better-defaults)))
+ '(pdf-view-midnight-colors (quote ("#655370" . "#fbf8ef")))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#c82829")
+     (40 . "#f5871f")
+     (60 . "#eab700")
+     (80 . "#718c00")
+     (100 . "#3e999f")
+     (120 . "#4271ae")
+     (140 . "#8959a8")
+     (160 . "#c82829")
+     (180 . "#f5871f")
+     (200 . "#eab700")
+     (220 . "#718c00")
+     (240 . "#3e999f")
+     (260 . "#4271ae")
+     (280 . "#8959a8")
+     (300 . "#c82829")
+     (320 . "#f5871f")
+     (340 . "#eab700")
+     (360 . "#718c00"))))
+ '(vc-annotate-very-old-color nil)
+ '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -58,11 +93,24 @@
  ;; If there is more than one, they won't work right.
  )
 
-(setq package-list '(better-defaults solarized-theme))
+(setq package-list '(better-defaults chyla))
 
 ;; Show line numbers
 (global-linum-mode)
 (global-hl-line-mode)
+
+(require 'linum)
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+          (ceiling (* (if (boundp 'text-scale-mode-step)
+                  (expt text-scale-mode-step
+                    text-scale-mode-amount) 1)
+              (if (car (window-margins))
+                  (car (window-margins)) 1)
+              ))))
+(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
+
 
 ;; Typography
 (set-face-attribute 'default nil
@@ -123,7 +171,7 @@
 
 (add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
 
-(load-theme 'spacemacs-light t)
+(load-theme 'chyla t)
 
 (when (memq window-system '(mac ns x))
 (exec-path-from-shell-initialize))
@@ -131,3 +179,4 @@
 (projectile-rails-global-mode)
 
 (treemacs)
+
