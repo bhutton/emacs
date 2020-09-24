84dae19e17a9adbc8ee93ceb1c59a9046b946c15
Ben Hutton <ben@benhutton.com.au>
2020-09-24 13:37:23 +0800

    Reassignment treemac window key.

--- init.el
+++ init.el
@@ -499,7 +499,7 @@ the current position of point, then move it to the beginning of the line."
 (global-set-key (kbd "M-<down>") 'windmove-delete-down)
 (global-set-key (kbd "s-<s-right>") 'move-end-of-line)
 (global-set-key (kbd "s-<s-left>") 'smart-line-beginning)
-(global-set-key (kbd "C-1") 'treemacs-select-window)
+(global-set-key (kbd "s-1") 'treemacs-select-window)
 
 (define-key prog-mode-map (kbd "M-RET") 'emr-show-refactor-menu)
 (define-key prog-mode-map (kbd "C-t") 'rspec-verify-all)
